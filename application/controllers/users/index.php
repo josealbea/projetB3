71b174dea17c213b53576a2e7c702ddb514b0880
José <jose.albea@gmail.com>
2012-12-07 13:31:04 +0100

    controller user

--- application/controllers/users/index.php
+++ application/controllers/users/index.php
@@ -86,6 +86,6 @@ function do_post() {
 	else {
 		extract($_POST);
 		$membre = new Application_Model_Users();
-		$addUser = $membre->addUser($pseudo, $password, $mail, $nom, $prenom, $ville, $code_postal, $telephone     );
+		$addUser = $membre->addUser($pseudo, $password, $mail, $nom, $prenom, $ville, $code_postal, $telephone);
 	}
 }
