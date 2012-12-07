672ada5a37a59f1b7be70d5a8d1eabbe6772bab8
José <jose.albea@gmail.com>
2012-12-07 13:48:32 +0100

    Aajout de l'envoi de mail

--- application/models/Users.php
+++ application/models/Users.php
@@ -28,15 +28,69 @@ class Application_Model_Users {
 		}
 	}
 
-	function addUser($pseudo, $password, $mail, $nom, $prenom, $ville, $code_postal, $telephone); { // a faire
+	function addUser($pseudo, $password, $mail, $nom, $prenom, $ville, $code_postal, $telephone); {
+            global $bdd;
 		try {
-			$sql = $bdd->prepare("INSERT INTO membre VALUES :data");
-			$sql->bindValue(":data", $data);
-			$sql->execute();
+			$sql = $bdd->prepare("INSERT INTO membre (id_membre, pseudo, password, mail, nom, prenom, ville, code_postal, telephone, type, statut) VALUES (NULL, ':pseudo', ':password', ':mail', ':nom', ':prenom', ':ville', ':code_postal', ':telephone', '2', '0')");
+			$sql->bindValue(":pseudo", $pseudo);
+                        $sql->bindValue(":password", $password);
+                        $sql->bindValue(":mail", $mail);
+                        $sql->bindValue(":nom", $nom);
+                        $sql->bindValue(":prenom", $prenom);
+                        $sql->bindValue(":ville", $ville);
+                        $sql->bindValue(":code_postal", $code_postal);
+                        $sql->bindValue(":telephone", $telephone);
+			$result = $sql->execute();
+                        if ($result) {
+                            sendEmail($mail);
+                            echo "Le compte vient d'être créé. Toutefois, il est nécéssaire de le valider via le mail qui vous a été envoyé."
+                        }
 		}
 		catch (PDOEXCEPTION $e) {
 			die('Erreur : '.$e->getMessage());
 		}
 	}
+        
+        function sendEmail($mail) {
+            global $bdd;
+            try {
+                $sql = $bdd->prepare("SELECT * FROM membre WHERE mail = ':mail'");
+                $sql->bindValue(":mail", $mail);
+                $result = $sql->execute();
+                if ($result) {
+                    $to  = $mail;
+                    $subject = SITE_NAME." - Validation de votre compte";
+                    $message = '
+                    <html>
+                     <head>
+                      <title>Calendrier des anniversaires pour Août</title>
+                     </head>
+                     <body>
+                      <p>Voici les anniversaires à venir au mois d\'Août !</p>
+                      <table>
+                       <tr>
+                        <th>Personne</th><th>Jour</th><th>Mois</th><th>Année</th>
+                       </tr>
+                       <tr>
+                        <td>Josiane</td><td>3</td><td>Août</td><td>1970</td>
+                       </tr>
+                       <tr>
+                        <td>Emma</td><td>26</td><td>Août</td><td>1973</td>
+                       </tr>
+                      </table>
+                     </body>
+                    </html>
+                    ';
+                    $headers  = 'MIME-Version: 1.0' . "\r\n";
+                    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+                    $headers .= 'Reply-To: José de'.SITE_NAME.'<jose.albea@gmail.com>' ."\r\n";
+                    $headers .= 'Service inscription '.SITE_NAME."\r\n";
+                    mail($to, $subject, $message, $headers);
+                }
+            }
+            catch(PDOException $e) {
+                die('Erreur : '.$e->getMessage());
+            }
+        }
 
 }
