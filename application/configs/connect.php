5cebffb2cfc149e23f0b0bb5a1100682381cc0f5
josealbea <jose.albea@gmail.com>
2012-12-21 17:07:39 +0100

    Maj id bdd pour achetervehicule.com

--- application/configs/connect.php
+++ application/configs/connect.php
@@ -1,6 +1,6 @@
 <?php
 	try {
-		$bdd = new PDO('mysql:host=localhost;dbname=vehicule', 'root', 'root');
+		$bdd = new PDO('mysql:host=db441597014.db.1and1.com;dbname=db441597014', 'dbo441597014', 'f4qSY2dE');
 	}
 	catch (PDOException $e) {
 		die('Erreur : '.$e->getMessage());
