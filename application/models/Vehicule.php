80c6f0f8652a691dae6bcb2057bdafdb97aef074
josealbea <jose.albea@gmail.com>
2013-02-09 20:22:14 +0100

    j

--- application/models/Vehicule.php
+++ application/models/Vehicule.php
@@ -72,11 +72,11 @@ class Application_Model_Vehicule {
 		global $bdd;
          $wheres = array();
         if ($type_vehicule != "") {
-            $wheres[] = "id_categorie=".$type_vehicule;
+            $wheres[] = "id_categorie=$type_vehicule";
         }
         $where = (count($wheres) == 0) ? "" : " WHERE " . join(" AND ", $wheres);
 		try {
-            $sql = $bdd->prepare("SELECT * FROM vehicule ."$where." ORDER BY date_ajout DESC LIMIT :limit_min, :limit_max");
+            $sql = $bdd->prepare("SELECT * FROM vehicule $where ORDER BY date_ajout DESC LIMIT :limit_min, :limit_max");
             $sql->bindValue(":limit_min", $limit_min,  PDO::PARAM_INT);
             $sql->bindValue(":limit_max", $limit_max,  PDO::PARAM_INT);
             $result = $sql->execute();
